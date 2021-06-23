8be3ebe143f6011098cdf0ebaeb5eb07262b2736
Jonathan Quintero Giraldo <jonathan.quintero@newshore.es>
2021-06-22 20:07:55 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { tasks: [] }
+  }  
+
+  sendData = (event) => {
+    event.preventDefault()      
+    this.setState({       
+      tasks: [...this.state.tasks, event.target.formTask.value]      
+    })   
+  }
+
   render() {
+    console.log(this.state.tasks)
     return (
       <div className="wrapper">
         <div className="list">
@@ -12,9 +25,12 @@ class App extends Component {
             <li>Sacar la ropa</li>
             <li>Hacer la cama</li>
             <li>Leer un rato</li>
+            {this.state.tasks.map((name, index) =>
+            <li key={index}>{ name }</li>
+          )} 
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+           <form onSubmit={ this.sendData }>
+             <input type="text" id="new-task" name="formTask" placeholder="Ingresa una tarea y oprime Enter" />
            </form>
         </div>
       </div>
